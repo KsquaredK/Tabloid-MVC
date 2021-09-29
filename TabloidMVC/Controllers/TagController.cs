d638778781bedcae429aa167eee586f94d5af967
Matt <Mattxiong_14@hotmail.com>
2021-09-29 11:52:26 -0500

    added all tagcontroller, tag model, tag repository, itag repository and a view for index of tags to make the list of tags display. Also edited the nav bar so you can click on tag management and it directs you to the tags index.

---
+++ TabloidMVC/Controllers/TagController.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TabloidMVC.Models;
+using TabloidMVC.Repositories;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+
+
+
+namespace TabloidMVC.Controllers
+{
+    public class TagController : Controller
+    {
+        private readonly ITagRepository _tagRepository;
+
+        public TagController(ITagRepository tagRepository)
+        { 
+            _tagRepository = tagRepository;
+        }
+        // GET: TagController
+        [Authorize]
+        public ActionResult Index()
+        {
+            List<Tag> tags = _tagRepository.GetAllTags();
+
+            return View(tags);
+        }
+
+        //// GET: CategoryController/Details/5
+        //public ActionResult Details(int id)
+        //{
+
+        //    return View();
+        //}
+
+        //// GET: CategoryController/Create
+        //[Authorize]
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        //// POST: CategoryController/Create
+        //[Authorize]
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create(Category category)
+        //{
+        //    try
+        //    {
+        //        _categoryRepository.AddCategory(category);
+        //        return RedirectToAction("Index");
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine(ex.Message);
+        //        return View(category);
+        //    }
+        //}
+
+        //// GET: CategoryController/Edit/5
+        //[Authorize]
+        //public ActionResult Edit(int id)
+        //{
+        //    var category = _categoryRepository.GetCategoryById(id);
+        //    return View(category);
+        //}
+
+        //// POST: CategoryController/Edit/5
+        //[Authorize]
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit(int id, Category category)
+        //{
+        //    try
+        //    {
+        //        _categoryRepository.UpdateCategory(category);
+        //        return RedirectToAction("Index");
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine(ex.Message);
+        //        return View(category);
+        //    }
+        //}
+
+        //// GET: CategoryController/Delete/5
+        //[Authorize]
+        //public ActionResult Delete(int id)
+        //{
+        //    Category category = _categoryRepository.GetCategoryById(id);
+        //    return View(category);
+        //}
+
+        //// POST: CategoryController/Delete/5
+        //[Authorize]
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Delete(int id, Category category)
+        //{
+        //    try
+        //    {
+        //        category = _categoryRepository.GetCategoryById(id);
+        //        _categoryRepository.DeleteCategory(id);
+        //        return RedirectToAction("Index");
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine(ex.Message);
+        //        return View(category);
+        //    }
+        //}
+    }
+}
---
+++ TabloidMVC/Repositories/TagRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TabloidMVC.Models;
+
+
+namespace TabloidMVC.Repositories
+{
+    public class TagRepository : BaseRepository, ITagRepository
+    {
+        public TagRepository(IConfiguration config) : base(config) { }
+        public List<Tag> GetAllTags()
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @" 
+                        SELECT Id, [Name] 
+                        FROM Tag 
+                        ORDER BY Name";
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    List<Tag> tags = new List<Tag>();
+
+                    while (reader.Read())
+                    {
+                        Tag tag = new Tag
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("name")),
+                        };
+                        tags.Add(tag);
+                    }
+
+                    reader.Close();
+
+                    return tags;
+                }
+            }
+        }
+    }
+}
